bb5ffe1608537d3a724b6081a01b0fd917e99ed5
L Gabriel Miller <gmiller2007@gmail.com>
2018-09-22 17:15:57 -0400

    First pass at implementing aot compilation to shrink over the wire payload.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
 const path = require('path');
 const copyPlugin = require('copy-webpack-plugin');
+const AotPlugin = require('@ngtools/webpack').AoTPlugin;
+
+const prodPlugins = [
+    new AotPlugin({
+      tsConfigPath: 'tsconfig-prod.json',
+      entryModule: path.resolve(__dirname, 'src/app/app.module#AppModule')
+    }),
+   ];
+
+const IS_DEV = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    entry: './static/ts/main.ts',
+    entry: './static/ts/' + path.join(root.src, IS_DEV ? 'main.ts' : 'main-prod.ts'),
     mode: 'production',
     module: {
         rules: [
             {
-                test: /\.ts?$/,
-                use: 'ts-loader',
+                test: /\.tsx?$/,
+                use: IS_DEV ? ['ts-loader', 'angular2-template-loader'] : '@ngtools/webpack',
+                exclude: IS_DEV ? [/node_modules/] : []
+            /*},
+            {
+                test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
+                loader: '@ngtools/webpack'*/
             }
         ]
     },
@@ -19,9 +34,18 @@ module.exports = {
     plugins: [
         new copyPlugin([
             { from: './node_modules/zone.js/dist/zone.min.js', to: 'zone.min.js'}
-        ])
+        ]),
+        new AotPlugin({
+            tsConfigPath: './tsconfig.json',
+            entryModule: path.resolve(__dirname, 'static/ts/app.module#AppModule'),
+            sourceMap: true
+        })
     ],
     resolve: {
         extensions: ['.ts', '.js'],
     },
+    watch: true,
+    watchOptions: {
+        ignored: ['node_modules'],
+    }
 }
