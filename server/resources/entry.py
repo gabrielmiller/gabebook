14789ee4ca63e27a640a15050056b0498175abb3
L Gabriel Miller <gmiller2007@gmail.com>
2018-12-16 15:59:19 -0500

    Adds dummy identify and authorize decorators to resource declarations to prevent unauthenticated access and unpermitted access.

--- server/resources/entry.py
+++ server/resources/entry.py
@@ -1,10 +1,17 @@
+from falcon_autocrud.auth import identify, authorize
 from falcon_autocrud.resource import CollectionResource, SingleResource
+
+from auth import Authenticate, Identify
 from models import entry
 
+@identify(Identify)
+@authorize(Authenticate)
 class EntryCollectionResource(CollectionResource):
     methods = ['GET', 'POST']
     model = entry.Entry
 
+@identify(Identify)
+@authorize(Authenticate)
 class EntryResource(SingleResource):
     methods = ['DELETE', 'PATCH']
     model = entry.Entry
--- server/resources/person.py
+++ server/resources/person.py
@@ -1,11 +1,18 @@
+from falcon_autocrud.auth import identify, authorize
 from falcon_autocrud.resource import CollectionResource, SingleResource
+
+from auth import Authenticate, Identify
 from models import person
 
+@identify(Identify)
+@authorize(Authenticate)
 class PersonCollectionResource(CollectionResource):
     allow_subresources = False
     methods = ['GET', 'POST']
     model = person.Person
 
+@identify(Identify)
+@authorize(Authenticate)
 class PersonResource(SingleResource):
     allow_subresources = False
     methods = ['DELETE', 'PATCH']
